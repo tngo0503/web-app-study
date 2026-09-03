2d1559e4377377d152d8fd617e2d298366b5263d
tngo0503 <truongngo0503@gmail.com>
2026-09-03 13:23:00 +0700

    wip: course/note CRUD logic, known bugs in pushNote and printNotes

--- backend/blogProject/test.js
+++ backend/blogProject/test.js
@@ -1,7 +1,7 @@
 const courses = [];
 
 function isCourseExist(courseName) {
-  return courses.some(eachCourse => eachCourse.course === courseName);
+  return courses.some(eachCourse => eachCourse.courseName === courseName);
 }
 
 function pushCourse(courseName) {
@@ -19,9 +19,9 @@ function pushCourse(courseName) {
 function pushNote(courseName, title, content){
   if(!isCourseExist(courseName))return false;
   const newNote = createNote(title, content);
-  courses.find(course => {
-    if(course.courseName === courseName){
-      course.notes.push(newNote)
+  courses.find(eachCourse => {
+    if(eachCourse.courseName === courseName){
+      eachCourse.notes.push(newNote);
     }
   })
   return true;
@@ -38,14 +38,41 @@ function createNote(title, content) {
     createOn: formattedDate,
     content,
     describe() {
-      return `\tTitle: ${title}\n\tCreate on: ${this.createOn}\n\tNote:\n\n${content}`;
+      return `\tTitle: ${title}\n\tCreate on: ${this.createOn}\n\tNote:\n\n${content}\n`;
     }
   }
 };
 
-const test = createNote('Intro to database', 'const {name} = courses where {} is destructuring which is a fancy way of just slicing the property "name" of the object "courses"');
-console.log(test.describe());
+function getCourse(courseName){
+    return courses.find(eachCourse => eachCourse.courseName === courseName);
+}
+
+function printNotes(courseName){
+    try {
+        const course = getCourse(courseName);
+        if(course){
+            for(let i = 0; i < course.notes.length; ++i){
+                console.log(course.notes[i].describe());
+            }
+        }
+        else throw error;
+    } catch (error) {
+        console.log("Course name does not exist\nprintNote() function in test.js file");
+    }
+}
+
+// const test = createNote('Intro to database', 'const {name} = courses where {} is destructuring which is a fancy way of just slicing the property "name" of the object "courses"');
+// console.log(test.describe());
+
+pushCourse('CS101');
+pushNote('CS101', 'Intro to database', 'const {name} = courses where {} is destructuring which is a fancy way of just slicing the property "name" of the object "courses"')
+
+pushCourse('CS100');
+pushNote('CS100', 'Intro to database', 'const {name} = courses where {} is destructuring which is a fancy way of just slicing the property "name" of the object "courses"')
+pushNote('CS100', 'Intro to programming', 'const {name} = courses where {} is destructuring which is a fancy way of just slicing the property "name" of the object "courses"')
+pushNote('CS100', 'Intro to search engine', 'const {name} = courses where {} is destructuring which is a fancy way of just slicing the property "name" of the object "courses"')
 
 
 
-console.log(isCourseExist())
+printNotes('CS103');
+
--- backend/blogProject/utils/courses.js
+++ backend/blogProject/utils/courses.js
@@ -1,22 +1,80 @@
 const courses = [];
 
+function isCourseExist(courseName) {
+  return courses.some(eachCourse => eachCourse.courseName === courseName);
+}
+
+function pushCourse(courseName) {
+  if(isCourseExist(courseName)){
+    return false;
+  }
+  const newCourse = {
+    courseName: courseName,
+    notes: []
+  };
+  courses.push(newCourse);
+  return true;
+}
+
+function pushNote(courseName, title, content){
+  if(!isCourseExist(courseName))return false;
+  const newNote = createNote(title, content);
+  courses.find(eachCourse => {
+    if(eachCourse.courseName === courseName){
+      eachCourse.notes.push(newNote);
+    }
+  })
+  return true;
+}
 
 function createNote(title, content) {
   let formattedDate = new Date().toLocaleString('en-US', {
     timeZone: 'America/Los_Angeles',
-    timeStyle: 'medium',  // "2:13:48 AM"
-    dateStyle: 'full'     // "Tuesday, September 1, 2026"
+    timeStyle: 'medium', // "2:13:48 AM"
+    dateStyle: 'full' // "Tuesday, September 1, 2026"
   })
   return {
     title,
     createOn: formattedDate,
     content,
     describe() {
-      return `\tTitle: ${title}\n\tCreate on: ${this.createOn}\n\tNote:\n\n${content}`;
+      return `\tTitle: ${title}\n\tCreate on: ${this.createOn}\n\tNote:\n\n${content}\n`;
     }
   }
 };
 
+function getCourse(courseName){
+    return courses.find(eachCourse => eachCourse.courseName === courseName);
+}
+
+function printNotes(courseName){
+    try {
+        const course = getCourse(courseName);
+        if(course){
+            for(let i = 0; i < course.notes.length; ++i){
+                console.log(course.notes[i].describe());
+            }
+        }
+        else throw error;
+    } catch (error) {
+        console.log("Course name does not exist\nprintNote() function in test.js file");
+    }
+}
+
+export {}
+
+// const test = createNote('Intro to database', 'const {name} = courses where {} is destructuring which is a fancy way of just slicing the property "name" of the object "courses"');
+// console.log(test.describe());
+
+// pushCourse('CS101');
+// pushNote('CS101', 'Intro to database', 'const {name} = courses where {} is destructuring which is a fancy way of just slicing the property "name" of the object "courses"')
+
+// pushCourse('CS100');
+// pushNote('CS100', 'Intro to database', 'const {name} = courses where {} is destructuring which is a fancy way of just slicing the property "name" of the object "courses"')
+// pushNote('CS100', 'Intro to programming', 'const {name} = courses where {} is destructuring which is a fancy way of just slicing the property "name" of the object "courses"')
+// pushNote('CS100', 'Intro to search engine', 'const {name} = courses where {} is destructuring which is a fancy way of just slicing the property "name" of the object "courses"')
 
 
-export {courses}; 
+
+// printNotes('CS103');
+
